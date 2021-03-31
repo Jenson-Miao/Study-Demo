dc8597a4f5ed05d398e857398d5fbc2529c2d13b
Jenson <1763417179@qq.com>
2021-03-31 18:26:50 +0800

    完成一个接口

--- projects/personal-blog/blog-java/blog-article/src/main/java/com/miaojingu/blog/article/controller/CategoryController.java
+++ projects/personal-blog/blog-java/blog-article/src/main/java/com/miaojingu/blog/article/controller/CategoryController.java
@@ -1,6 +1,14 @@
 package com.miaojingu.blog.article.controller;
 
 
+import com.miaojingu.blog.article.req.CategoryREQ;
+import com.miaojingu.blog.article.service.ICategoryService;
+import com.miaojingu.blog.util.base.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Jenson Miao
  * @since 2021-03-30
  */
-@RestController
-@RequestMapping("//category")
+@Api(value="分类管理接口",description="分类管理接口，提供分类的增删改查") //Swagger
+@RestController //所有方法都会返回json字符串进行响应
+@RequestMapping("/category")
 public class CategoryController {
 
+    @Autowired
+    private ICategoryService iCategoryService;
+
+    /**
+     * 分页条件查询分类信息
+     * @param req
+     * @return
+     */
+    @ApiOperation("根据分类名称与状态查询分类列表接口") //Swagger对接口的介绍
+    @PostMapping("/search") //请求路径： /category/search
+    public Result search(@RequestBody CategoryREQ req){
+
+        return iCategoryService.queryPage(req);
+    }
 }
--- projects/personal-blog/blog-java/blog-article/src/main/java/com/miaojingu/blog/article/service/impl/CategoryServiceImpl.java
+++ projects/personal-blog/blog-java/blog-article/src/main/java/com/miaojingu/blog/article/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,14 @@
 package com.miaojingu.blog.article.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.miaojingu.blog.article.req.CategoryREQ;
 import com.miaojingu.blog.entities.Category;
 import com.miaojingu.blog.article.mapper.CategoryMapper;
 import com.miaojingu.blog.article.service.ICategoryService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.miaojingu.blog.util.base.Result;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,6 +24,21 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
 
     @Override
     public Result queryPage(CategoryREQ req) {
-        return null;
+        // 创建查询条件对象
+        QueryWrapper<Category> wrapper = new QueryWrapper<>();
+        // 查看是否有分类名称，并添加条件
+        if(StringUtils.isNotEmpty(req.getName())){
+            wrapper.like("name",req.getName());
+        }
+        // 查看是否有分类状态，并添加条件
+        if(req.getStatus()!= null){
+            wrapper.like("status",req.getStatus());
+        }
+        // 先根据status降序排列，然后按照sort升序排列
+        wrapper.orderByDesc("status").orderByAsc("sort");
+
+        // 第一个参数为分页对象，第二个参数为查询条件
+        IPage<Category> categoryIPage = baseMapper.selectPage(req.getPage(),wrapper);
+        return Result.ok(categoryIPage);
     }
 }
